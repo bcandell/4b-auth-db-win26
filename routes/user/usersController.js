43b9bc20d3d0bfa10de4d4d05f3fe4f53a0f4b48
bcandell <bcandell@gmail.com>
2026-01-12 12:11:24 -0500

    Add encryption for user creation

--- routes/user/usersController.js
+++ routes/user/usersController.js
@@ -1,8 +1,18 @@
 const User = require("./usersModel");
+const bcrypt = require("bcrypt");
 
 const createUser = async (userData) => {
     try {
-        const newUser = await User.create(userData);
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+        const secureUserData = {
+            username: userData.username,
+            password: hashedPassword
+        }
+
+        const newUser = await User.create(secureUserData);
+        const returnedUser = User.findOne({ username: userData.username }).select("-password");
         return newUser;
     } catch (error) {
         throw error;
--- routes/user/usersModel.js
+++ routes/user/usersModel.js
@@ -8,7 +8,9 @@ const userSchema = new mongoose.Schema({
     }, 
     password: {
         type: String,
-        required: true
+        required: true,
+        //prevents password return 
+        select: false
     }
 }, {
     timestamps: true
